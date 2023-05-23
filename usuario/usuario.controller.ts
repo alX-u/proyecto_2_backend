f8b622da538560ac738224524a01f8cf876f8d8a
ALEJANDRO VERTEL VILLEGA <avertel@TRANSELCA.COM.CO>
2023-05-23 15:37:20 -0500

    Added create, getById and delete to User

--- usuario/usuario.controller.ts
+++ usuario/usuario.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import User from "./usuario.model"; // Importa el modelo de usuario definido en Mongoose
 
-// Controlador para la creación de usuarios
+// Creación de usuarios
 export const createUser = async (req: Request, res: Response) => {
   try {
     const { name, password, email, phone_number, address, role } = req.body; // Obtén las propiedades del cuerpo de la solicitud
@@ -24,3 +24,39 @@ export const createUser = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Error al crear el usuario", error });
   }
 };
+
+//Obtener usuario
+export async function getUserById(req: Request, res: Response) {
+  try {
+    //Para usuario admin
+    const { _id } = req.params;
+
+    const user = await User.findOne({ _id: _id, active: true });
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error al obtener el usuario" });
+  }
+}
+
+//Borrar usuarios
+export async function deleteUser(req: Request, res: Response) {
+  //Aquí uso params
+  const { _id } = req.params;
+
+  try {
+    //El usuario se inhabilita, en vez de borrarse
+    const deletedUser = await User.findOneAndUpdate(
+      { _id: _id, active: true },
+      { active: false }
+    );
+    if (!deletedUser)
+      return res.status(404).json({ message: "Usuario no encontrado" });
+
+    res.status(200).json({ message: "Usuario inhabilitado correctamente." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error al eliminar el usuario" });
+  }
+}
--- usuario/usuario.routes.ts
+++ usuario/usuario.routes.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import { createUser } from "./usuario.controller";
+import { createUser, deleteUser, getUserById } from "./usuario.controller";
 
 const router = express.Router();
 
 // Ruta para crear un usuario
 router.post("/", createUser);
+
+//Ruta para obtener un usuario según el ID
+router.get("/ById/:_id", getUserById);
+
+//Ruta para inhabilitar al usuario según el ID
+router.delete("/:_id", deleteUser);
 
 export default router;
