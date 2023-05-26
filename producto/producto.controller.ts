47da914e51a393674951f3aa90c8a8a409a51caa
ALEJANDRO VERTEL VILLEGA <avertel@TRANSELCA.COM.CO>
2023-05-26 12:27:27 -0500

    Ok

--- producto/producto.controller.ts
+++ producto/producto.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import Product from "./producto.model"; // Importa el modelo de usuario definido en Mongoose
+import Product, { IProduct } from "./producto.model"; // Importa el modelo de usuario definido en Mongoose
 import { generateToken } from "../auth/auth_token";
 
 //Creación de productos
@@ -30,20 +30,21 @@ export async function getProductById(req: Request, res: Response) {
   try {
     const { _id } = req.params;
 
-    const product = await Product.findOne({ _id: _id, active: true });
+    const producto = await Product.findOne({ _id: _id, active: true });
 
-    res.status(200).json(product);
+    res.status(200).json(producto);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Error al obtener el usuario" });
+    res.status(500).json({ message: "Error al obtener el producto" });
   }
 }
 
-//Obtener productos por Restaurante y/o por categoría
+//Obtener productos por usuario, texto de búsqueda y/o por categoría
 export async function getProductsbyCategoryAndUser(
   req: Request,
   res: Response
 ) {
+  //Falta agregar lo del texto de búsqueda
   try {
     const { user, category } = req.query;
     const filtro: any = {};
@@ -53,10 +54,29 @@ export async function getProductsbyCategoryAndUser(
     if (category) {
       filtro.category = category;
     }
+    //Buscamos solo los que estén activos
+    filtro.active = true;
     const productos = await Product.find(filtro);
     res.status(200).json(productos);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Error al obtener los productos" });
+  }
+}
+
+//Obtener categorías de los productos de un usuario proveído
+export async function getProductCategoriesByUser(req: Request, res: Response) {
+  try {
+    const { userId } = req.params;
+
+    const productos = await Product.find({ user: userId, active: true });
+    const categories = productos.map((producto: IProduct) => producto.category);
+
+    res.status(200).json(categories);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Error al obtener las categorías de productos" });
   }
 }
