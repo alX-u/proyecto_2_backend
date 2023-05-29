1822408c83075070eac0a4f892c7684352029d54
Alejandro V <70179001+alX-u@users.noreply.github.com>
2023-05-28 19:23:30 -0500

    Update usuario.test.ts

--- usuario/usuario.test.ts
+++ usuario/usuario.test.ts
@@ -1,9 +1,20 @@
 import { Request, Response } from "express";
 import request from "supertest";
+const mongoose = require("mongoose");
 import userRoutes from "./usuario.routes";
 import { describe, expect, it, test } from '@jest/globals';
 
 jest.useRealTimers();
+
+beforeEach(async () => {
+    const url = `mongodb+srv://vertel:h3nt3DTE804Kdx76@proyecto2backend.yunr3x4.mongodb.net/`;
+    await mongoose.connect(url);
+});
+
+/* Closing database connection after each test. */
+afterEach(async () => {
+    await mongoose.connection.close();
+});
 
 //Pruebas de creación de usuario
 describe('createUser', () => {
